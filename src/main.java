09e1b8fe4b536d1026ffb0c8f9c4c89add72892c
Juan-Ceballos <55723135+Juan-Ceballos@users.noreply.github.com>
2026-09-11 00:46:59 -0400

    sample get request using httprequest

--- src/main.java
+++ src/main.java
@@ -1,3 +1,8 @@
+import java.net.http.HttpRequest;
+
 public class main {
-    
+        HttpRequest getRequest = HttpRequest.newBuilder()
+            .uri(null)
+            .header("null", "null")
+            .build();
 }
